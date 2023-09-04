2ca26aad47fe9c62ef5e525470aaf63e9d1e9921
xglog <gcldzhc@163.com>
2023-09-05 00:33:43 +0800

    2023年9月5日

--- gonote/lessons_1/package.go
+++ gonote/lessons_1/package.go
@@ -37,7 +37,6 @@ git push -u origin main
 程序从 main 包开始运行。
 本程序通过导入路径 "fmt" 和 "math/rand" 来使用这两个包。
 按照约定，包名与导入路径的最后一个元素一致。例如，"math/rand" 包中的源码均以 package rand 语句开始。
-*注意：此程序的运行环境是固定的，因此 rand.Intn 总是会返回相同的数字
 */
 
 package main
